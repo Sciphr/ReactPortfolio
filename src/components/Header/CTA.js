90709dc2e08f3cf8eb84b76a27e1e82af9183fbc
Sciphr <68548543+Sciphr@users.noreply.github.com>
2022-09-05 16:48:05 -0400

    Added extra button

--- src/components/Header/CTA.js
+++ src/components/Header/CTA.js
@@ -7,6 +7,13 @@ const CTA = () => {
       <a href={Resume} download className="btn">
         Download Resume
       </a>
+      <a
+        href="https://sciphr.github.io/ResumeWebsite/"
+        target="__blank"
+        className="btn"
+      >
+        Visit Resume Website
+      </a>
       <a href="#contact" className="btn btn-primary">
         Send me a message!
       </a>
